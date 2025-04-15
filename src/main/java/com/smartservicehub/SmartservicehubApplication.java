e136bce4290101b2f6dd102abcbda5773286b5a2
Yuvaraj <yuvaraj.sf.java@gmail.com>
2025-04-15 15:10:22 +0530

    Initial project structure with required dependencies

---
+++ src/main/java/com/smartservicehub/SmartservicehubApplication.java
@@ -0,0 +1,13 @@
+package com.smartservicehub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartservicehubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartservicehubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/smartservicehub/SmartservicehubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smartservicehub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartservicehubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
